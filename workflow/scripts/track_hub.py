6158817922d8685bdf767a7339b572602f3fbcbb
Mitchell Robert Vollger <mrvollger@gmail.com>
2021-10-14 12:57:13 -0700

    adding views to the script

--- workflow/scripts/track_hub.py
+++ workflow/scripts/track_hub.py
@@ -213,6 +213,7 @@ view_format_int = """
         filter.score 5:1000
         filterByRange.score on
         filterLimits.score 0:1000
+        gene-conversion-by-sample-interact.endsVisisble two
 
 """
 view_format_int_sm = """
