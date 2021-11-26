90579e1661e80a06b15aa144014168968c1cd835
mrvollger <mrvollger@gmail.com>
2021-11-25 20:18:50 -0800

    making modes

--- run_gc_smk.sh
+++ run_gc_smk.sh
@@ -7,12 +7,14 @@ if [ $1 == "setup" ]; then
     snakemake --configfile $configfile --cores $threads --use-conda \
         -p --notemp \
         setup_realign "${@:3}"
+
+    cp results/CHM13_V1.1_v2.23/gene-conversion/realign/realign.bed config/premade_windows.bed 
 fi
 
 if [ $1 == "realign" ]; then
     snakemake --configfile $configfile --cores $threads --use-conda \
         gene_conversion \
-        --config gcwindows=results/CHM13_V1.1/gene-conversion/realign/realign.bed \
+        --config gcwindows=config/premade_windows.bed \
         -p --notemp \
         "${@:3}"
 fi
