3e9133fc0e880ae476f776ecce3fe295d19fd855
Mitchell Robert Vollger <mrvollger@gmail.com>
2021-11-26 20:19:55 -0800

    first step in two part pipeline, not sure how it will go

--- run_gc_smk.sh
+++ run_gc_smk.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-configfile=$2
+configfile=config/config.yaml
 threads=150
+snakemake --configfile $configfile --cores $threads --use-conda \
+    gene_conversion \
+    -p --notemp \
+    "$@"
 
+exit
+configfile=$2
 if [ $1 == "setup" ]; then
     snakemake --configfile $configfile --cores $threads --use-conda \
         -p --notemp \
         setup_realign "${@:3}"
 
-    cp results/CHM13_V1.1_v2.23/gene-conversion/realign/realign.bed config/premade_windows.bed 
+    cp results/CHM13_V1.1_v2.23/gene-conversion/realign/realign.bed config/premade_windows.bed
 fi
 
 if [ $1 == "realign" ]; then
